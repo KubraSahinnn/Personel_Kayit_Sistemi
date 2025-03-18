fef118ee05561f029dd501075cd98344b77faa12
Kübra Şahim <kubra.shnx@gmail.com>
2025-03-18 20:34:48 +0300

    Personel Kayıt Sistemi
    Masaüstü uygulamalar için geliştirilmiştir.

---
+++ Peersonel_Kayit/Peersonel_Kayit/FrmGiris.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Peersonel_Kayit
+{
+    partial class FrmGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.txtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(126, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // txtKullaniciAd
+            // 
+            this.txtKullaniciAd.Location = new System.Drawing.Point(272, 153);
+            this.txtKullaniciAd.Name = "txtKullaniciAd";
+            this.txtKullaniciAd.Size = new System.Drawing.Size(191, 36);
+            this.txtKullaniciAd.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Heebo", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(648, 59);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Personel Takip Sistemi Giriş Paneli";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(272, 202);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(191, 36);
+            this.txtSifre.TabIndex = 4;
+            this.txtSifre.UseSystemPasswordChar = true;
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.AutoSize = true;
+            this.btnGirisYap.Location = new System.Drawing.Point(307, 244);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(119, 38);
+            this.btnGirisYap.TabIndex = 5;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // FrmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(737, 339);
+            this.Controls.Add(this.btnGirisYap);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKullaniciAd);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmGiris";
+            this.Text = "FrmGiris";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.TextBox txtKullaniciAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.Button btnGirisYap;
+    }
+}
---
+++ Peersonel_Kayit/Peersonel_Kayit/Frmİstatistik.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peersonel_Kayit
+{
+    public partial class Frmİstatistik : Form
+    {
+        public Frmİstatistik()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti=new SqlConnection("Data Source=DESKTOP-GPOGES2\\MSSQLSERVER02;Initial Catalog=PersonelVeriTabani;Integrated Security=True;Encrypt=False");
+        private void Frmİstatistik_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut1 = new SqlCommand("Select count(*) From Tbl_Personel ", baglanti);
+            SqlDataReader dr1=komut1.ExecuteReader();
+            while(dr1.Read())
+            {
+                lblToplamPersonel.Text = dr1[0].ToString();
+            }
+            baglanti.Close();
+            
+            //Evli personel sayısı
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("Select count(*) From Tbl_Personel where PerDurum=1",baglanti);
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while(dr2.Read()) 
+            {
+                lblEvliPersonel.Text = dr2[0].ToString();
+            }
+            baglanti.Close();
+
+            //Bekar personel sayısı
+            baglanti.Open();
+            SqlCommand komut3 = new SqlCommand("Select count(*) From Tbl_Personel where PerDurum=0",baglanti);
+            SqlDataReader dr3 = komut3.ExecuteReader();
+            while(dr3.Read()) 
+            {
+                lblBekarPersonel.Text = dr3[0].ToString();
+            }
+            baglanti.Close();
+
+            //Toplam şehir sayısı
+            baglanti.Open();
+            SqlCommand komut4 = new SqlCommand("Select count(distinct(PerSehir)) From Tbl_Personel", baglanti);
+            SqlDataReader dr4 = komut4.ExecuteReader();
+            while(dr4.Read())
+            {
+                lblSehirSayisi.Text = dr4[0].ToString();
+            }
+            baglanti.Close();
+
+            //Toplam maaş 
+            baglanti.Open();
+            SqlCommand komut5 = new SqlCommand("Select sum(PerMaas) From Tbl_Personel",baglanti);
+            SqlDataReader dr5 = komut5.ExecuteReader();
+            while (dr5.Read())
+            {
+                lblToplamMaas.Text = dr5[0].ToString();
+            }
+            baglanti.Close();
+
+            //Ortalama maaş
+            baglanti.Open();
+            SqlCommand komut6 = new SqlCommand("Select avg(PerMaas) From Tbl_Personel", baglanti);
+            SqlDataReader dr6 = komut6.ExecuteReader();
+            while (dr6.Read())
+            {
+                lblOrtalamaMaas.Text = dr6[0].ToString();
+            }
+            baglanti.Close();
+        }
+    }
+}
